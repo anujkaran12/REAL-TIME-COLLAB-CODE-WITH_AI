1cc93583ebddcdc5d29f009727b2928da5adf1a3
anujkaran12 <your-new-email@example.comgit config --global user.name>
2025-09-25 20:58:47 +0530

    change in app for lazy loading

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,29 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import Loading from "./components/Utility/Loading/Loading";
 
-import Home from "./pages/Home/Home";
-import Navbar from "./components/Navbar/Navbar";
-
-import Dashboard from "./pages/Dashboard/Dashboard";
-import RoomPage from "./pages/RoomPage/RoomPage";
-import PageNotFound from "./components/Utility/PageNotFound/PageNotFound";
+// Lazy imports
+const Home = lazy(() => import("./pages/Home/Home"));
+const Navbar = lazy(() => import("./components/Navbar/Navbar"));
+const Dashboard = lazy(() => import("./pages/Dashboard/Dashboard"));
+const RoomPage = lazy(() => import("./pages/RoomPage/RoomPage"));
+const PageNotFound = lazy(() => import("./components/Utility/PageNotFound/PageNotFound"));
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Dashboard" element={<RoomPage />} />
-        {/* <Route path="/RoomPage" element={<RoomPage />} /> */}
-        <Route path="/Room" element={<Dashboard />} />
-        <Route path="*" element={<PageNotFound />} />
-
-      </Routes>
-    </>
+    <BrowserRouter>
+      <Suspense fallback={<Loading/>}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Dashboard" element={<RoomPage />} />
+          {/* <Route path="/RoomPage" element={<RoomPage />} /> */}
+          <Route path="/Room" element={<Dashboard />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
