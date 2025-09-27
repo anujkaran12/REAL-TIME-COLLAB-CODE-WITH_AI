2216716587fffeda532c2bf49cd404a291ac645e
anujkaran12 <your-new-email@example.comgit config --global user.name>
2025-09-27 13:27:22 +0530

    new 2 dist

--- server/dist/index.js
+++ server/dist/index.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express_1.default.json());
 app.use((0, cors_1.default)({
-    origin: ["http://localhost:3000", "https://real-time-collab-code-with-ai.vercel.app", process.env.FRONTEND_URL],
+    origin: ["http://localhost:3000", process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
